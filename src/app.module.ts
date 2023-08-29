05449774b88880244243bfb2b9f5e6684d4e12ae
Manuchehr <manuchekhr.3232@gmail.com>
2023-08-29 20:16:07 +0500

    feat: add cache module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.register({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
